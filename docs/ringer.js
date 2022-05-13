ebf130e3411877bba799fbb56d32f17e57958d53
Brian Loudin <loudinb@icloud.com>
2022-05-13 18:14:55 -0400

    ringer

--- docs/ringer.js
+++ docs/ringer.js
@@ -79,7 +79,7 @@ var ringer = {
       $r.ctx.clearRect($r.actual_size*-0.5,$r.actual_size*-0.5,$r.actual_size,$r.actual_size);
   
       // first circle
-      $r.ctx.strokeStyle = "rgba(128,128,128,0.2)";
+      $r.ctx.strokeStyle = "rgba(255,128,128,0.2)";
       $r.ctx.beginPath();
       $r.ctx.arc(0,0,$r.r_size/2,0,2 * Math.PI, 2);
       $r.ctx.lineWidth =$r.r_thickness;
